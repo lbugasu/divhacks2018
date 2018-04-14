c81ef29e009c3e1a6a567b80614ce7d7ccacb1a7
lbugasu <lbugasu@gmail.com>
2018-04-14 16:56:52 -0400

    1.2
    Implements analyzing testing versus training accuracy for a given threshold value

--- src/divhacks2018/App.java
+++ src/divhacks2018/App.java
@@ -10,55 +10,76 @@ import java.util.*;
  *
  */
 public class App {
-	public static void main (String[]args) {
+	public static void main (String[]args) throws UnsupportedEncodingException {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What threshold do you want to analyze the  dataset?");
+		int threshold = in.nextInt();
+		
+		String testSet = "MNIST_test.csv";
+		String trainSet = "MNIST_train.csv";
+		
+		System.out.println("Testing Accuracy with selected Threshold:");
+		analyzeData(testSet, threshold);
 
+		System.out.println("\n\nTraining Accuracy with selected Threshold:");
+		analyzeData(trainSet, threshold);
 	}
 	
 	/**
 	 * This method analyzes the test data set using a given threshold value by the user
-	 * @param threshold - A given threshold to determine which digits are "EASY" versus which values are "HARD"
+	 * @param threshold - A given threshold (As a percentage value) to determine which digits are "EASY" versus which values are "HARD"
 	 * 
 	 */
-	public void analyzeTestData(int threshold) {
+	public static void analyzeData(String dataset, int threshold) throws UnsupportedEncodingException {
+
+		File file = new File(dataset);//A new file object to hold the file being analyzed
 		
-		/*
-		 * First Analyze the sample dataset for thresholds of above 50%, 60%, 70%, 80% and report the results in a txt file
-		 */
-		String fname = "MNIST_test.csv";
-		File file = new File(fname);
+		double digits = 0;//variable to count total digits analyzed
+		
+		double countEASY = 0;//Variable to count how many digits are "EASY" to identify
+		
+		double countHARD = 0;//Variable to count how many digits are "HARD" to identify
+		
+		Scanner in = null;
 		
 		try {
-			Scanner in = new Scanner(file);
-			
-			//Bypass the first line that has labels
-			in.nextLine();
-			
-			
-			//Begin Analyzing the dataset
-			
-			while (in.hasNextLine()) {				
-				String line = in.nextLine();
-				String [] s = line.split(",");
-				int value = 0;
-				
-				for (int i = 0; i<s.length;i++) {
-					int id = 0;
-					if (i>0) {
-						value += Integer.parseInt(s[i]);
-						}//Add the predictions of the current digit
-					else {
-						id = Integer.parseInt(s[i]);
-						}
-				}
-			}
-		}
-		catch(FileNotFoundException c){
-			System.out.println("File Not found!!");
-		} 
-		
-		catch (IOException e) {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//Bypass the first line that has labels
+		in.nextLine();
+
+		//Begin Analyzing the dataset
+		while (in.hasNextLine()) {				
+			String line = in.nextLine();
+			String [] s = line.split(",");
+			int value = 0;
+			
+			for (int i = 0; i<s.length;i++) {
+				int id = 0;
+				if (i>0) {
+					value += Integer.parseInt(s[i]);
+					}//Add the predictions of the current digit
+				else {
+					id = Integer.parseInt(s[i]);
+					}
+			}
+			//If digit is above threshold
+			if ((value/21)*100>threshold) {
+				countEASY++;
+			}
+			//If digit is below threshold
+			else {
+				countHARD++;
+			}
+			digits ++;
+		}
+		
+		System.out.printf("With a threshold of %d gives:\n%.2f - easily identifiable digits\n%.2f - ohardly identifiable digits", threshold, (countEASY/digits)*100, (countHARD/digits)*100);
+		
 	}
 }
 
