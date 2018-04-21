5e8e6718f71c8b06aefe90638286e0e37a2367f1
lbugasu <lbugasu@gmail.com>
2018-04-21 11:00:41 -0400

    3.0
    This version implements the use of a two dimensional array to store the results from analyzing the test and/or training data

--- src/divhacks2018/App.java
+++ src/divhacks2018/App.java
@@ -1,17 +1,25 @@
-
 package divhacks2018;
-import static java.lang.Math.min;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 
-/**
- * This is my application for analyzing a dataset of MNIST
- * @author Laurence Ininda
- *
- */
-public class App {
-	public static void main (String[]args) throws UnsupportedEncodingException {
+import java.util.*;
+import java.lang.String;
+import java.io.*;
+
+public class App{
+	public static void main(String[]args){
+	
+		/*
+		 * We will store our data in a two dimensional Array, with the rows representing the digits
+		 * and the columns representing the array of results such that say we pick dataset[4], 
+		 * the first element in that array, i.e. dataset[4][0] represents the number of times digit 4 was predicted correct,
+		 * and dataset[4][1] will represent the number of times digit 4 was hard to identify(predicted wrongly)
+		 */
+		int[][] results = new int[10][];
+		for(int j = 0;j<results.length;j++) {
+			results[j] = new int[2];
+			for (int k=0;k<2;k++) {
+				//results[j][];
+			}
+		}
 		Scanner in = new Scanner(System.in);
 		System.out.println("What threshold do you want to analyze the  dataset?");
 		int threshold = in.nextInt();
@@ -20,28 +28,30 @@ public class App {
 		String trainSet = "MNIST_train.csv";
 		
 		System.out.println("Testing Accuracy with selected Threshold:");
-		analyzeData(testSet, threshold);
+		analyzeData(testSet, threshold, results);
 
-		System.out.println("\n\nTraining Accuracy with selected Threshold:");
-		analyzeData(trainSet, threshold);
+		//System.out.println("\n\nTraining Accuracy with selected Threshold:");
+		//analyzeData(trainSet, threshold, results);
 		
-	}
+		//Print out the results
+		System.out.printf("With a threshold of %d%% here are the results:\n",threshold);
+		System.out.printf("%-7s%-22s%-22s%-12s%-15s\n", "Digit", "Correct Predictions","False predictions","Percentage", "Classification");
+		for(int i=0;i<results.length;i++) {
+			double predictionvalue = (results[i][0]/(results[i][1]*1.0+results[i][0]*1.0))*100;
+			String classification = "EASY";
+			if(predictionvalue<threshold) {
+				classification = "HARD";
+			}
+			System.out.printf("%-7d%-22d%-22d%-12f%-15s\n", i, results[i][0], results[i][1], predictionvalue,classification);
+		}
+		
+		in.close();
 	
-	/**
-	 * This method analyzes the test data set using a given threshold value by the user
-	 * @param threshold - A given threshold (As a percentage value) to determine which digits are "EASY" versus which values are "HARD"
-	 * 
-	 */
-	public static void analyzeData(String dataset, int threshold) throws UnsupportedEncodingException {
+	}
+	public static void analyzeData(String dataset, int threshold, int[][] results) {
 
+		
 		File file = new File(dataset);//A new file object to hold the file being analyzed
-		
-		double digits = 0;//variable to count total digits analyzed
-		
-		double countEASY = 0;//Variable to count how many digits are "EASY" to identify
-		
-		double countHARD = 0;//Variable to count how many digits are "HARD" to identify
-		
 		Scanner in = null;
 		
 		try {
@@ -51,41 +61,31 @@ public class App {
 			e.printStackTrace();
 		}
 		
-		//Bypass the first line that has labels
+		//Bypass the first line that contains the headers
 		in.nextLine();
 
 		//Begin Analyzing the dataset
-		while (in.hasNextLine()) {				
+		while (in.hasNextLine()) {
 			String line = in.nextLine();
 			String [] s = line.split(",");
-			int value = 0;
-			ArrayList <Integer> labels = new ArrayList();
+			int predictionValue = 0;
 			//The first index contains the label of the MNIST value
-			int id = Integer.parseInt(s[0]);
+			int id = Integer.parseInt(s[1]);
 			
 			//See how predictable the digit is by analyzing how the digit performs against the 21 Algorithms
-			for (int i = 1; i<s.length;i++) {
-				int read = 0;
-				if (i>0) {
-					value += Integer.parseInt(s[i]);
-					}//Add the predictions of the current digit
-				else {
-					read = Integer.parseInt(s[i]);
-					}
-			}
-			//If digit is above threshold 
-			if ((value/21)*100>threshold) {
-				countEASY++;
-			}
-			//If digit is below threshold
-			else {
-				countHARD++;
-			}
-			digits ++;
-		}
-		
-		System.out.printf("With a threshold of %d gives:\n%.2f - easily identifiable digits\n%.2f - ohardly identifiable digits", threshold, (countEASY/digits)*100, (countHARD/digits)*100);
-		
-	}
-}
+			for (int i = 2; i<s.length;i++) {
+				
+				predictionValue += Integer.parseInt(s[i]);
 
+			}
+			//If digit is above threshold mark the digit predictability as easy otherwise mark as false
+			if ((predictionValue/21.0)*100>threshold) {
+				results[id][0] = results[id][0]+1;
+				}
+			else{
+				results[id][1] = results[id][1]+1;
+				}
+			}
+		}
+	}
+	
